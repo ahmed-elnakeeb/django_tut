0b7f07505e0e5df71a954691aeb5b9d69e1f83a5
root <ahmedelnakeeb2016@outlook.com>
2021-10-16 19:15:28 +0000

    add html files

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,10 @@ from . import views
 app_name="main"
 urlpatterns = [
     path('', views.homepage,name="homepage"),
+    path('register/', views.register,name="register"),
+    path('login/', views.login,name="login"),
+    path('profile/', views.profile,name="profile"),
+
+
     
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 from .models import tutorial
 # Create your views here.
 def homepage(request):
-    return render(request=request,template_name="main/home.html",context={"tutorials":tutorial.objects.all})
-    
+    return render(request=request,template_name="main/home.html",
+        context={"tutorials":tutorial.objects.all})
+        
+def profile(request):
+    return render(request=request,template_name="main/profile.html")
+
+def login(request):
+    return render(request=request,template_name="main/login.html")
+
+def register(request):
+    return render(request=request,template_name="main/register.html")
