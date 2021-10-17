41f9875f9f46db7d6a828a17c99a2db5c0f4a7b2
root <ahmedelnakeeb2016@outlook.com>
2021-10-17 06:43:35 +0000

    some error

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import tutorial
+from  django.contrib.auth.forms import UserCreationForm
+from  django.contrib.auth import  login,logout,authenticate
+from django.contrib import messages
+
 # Create your views here.
 def homepage(request):
     return render(request=request,template_name="main/home.html",
@@ -9,8 +13,23 @@ def homepage(request):
 def profile(request):
     return render(request=request,template_name="main/profile.html")
 
-def login(request):
-    return render(request=request,template_name="main/login.html")
+def signin(request):
+    return render(request=request,template_name="main/signin.html")
 
 def register(request):
-    return render(request=request,template_name="main/register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request,"congrates")
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                messages.error(request,f"{msg}:{form.error_messages[msg]}")
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "main/register.html",
+                  context={"form":form})
